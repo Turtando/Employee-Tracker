c133ab806f5b85a3234c8e50b4610fe4075f890c
Christopher Turton <christopheraturton@gmail.com>
2020-07-12 16:00:23 -0400

    schema and seeds added

--- employeeSchema.sql
+++ employeeSchema.sql
@@ -5,14 +5,14 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE employees (
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NULL,
-  last_name VARCHAR(30) NULL,
-  role_id INT NULL,
-  manager_id INT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY(role_id) REFERENCES roles(roleID),
-  FOREIGN KEY(manager_id) REFERENCES roles(roleID)
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
+    role_id INT NULL,
+    manager_id INT DEFAULT NULL,
+    CONSTRAINT fk_role FOREIGN KEY(role_id) REFERENCES roles(id),
+    CONSTRAINT fk_employee FOREIGN KEY(manager_id) REFERENCES roles(id), 
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
@@ -20,13 +20,14 @@ CREATE TABLE roles (
     title VARCHAR(30),
     salary DECIMAL(10, 4)
     department_id INT NULL,
-    FOREIGN KEY(department_id) REFERENCES department(departmentID)
+    CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE SET NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT, 
-
-    FOREIGN KEY(department_id) REFERENCES department(departmentID)
+    id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30),
+    PRIMARY KEY (id) 
 );
 
 SELECT * FROM employees;
---
+++ employeeSeeds.sql
@@ -0,0 +1,32 @@
+INSERT INTO employees (first_name, last_name, title, department_name, salary, manager)
+VALUES ("John", "Doe", "Sales Lead", "Sales", 100000, "Ashley Rodriguez");
+
+INSERT INTO employees (first_name, last_name, title, department_name, salary, manager)
+VALUES ("Mike", "Chan", "Salesperson", "Sales", 80000, "John Doe");
+
+INSERT INTO employees (first_name, last_name, title, department_name, salary, manager)
+VALUES ("Ashley", "Rodriguez", "Lead Engineer", "Engineering", 150000, "null"); 
+
+INSERT INTO employees (first_name, last_name, title, department_name, salary, manager)
+VALUES ("Kevin", "Tupik", "Software Engineer", "Engineering", 120000, "Ashley Rodriguez"); 
+
+INSERT INTO employees (first_name, last_name, title, department_name, salary, manager)
+VALUES ("Malia", "Brown", "Accountant", "Finance", 125000, "null"); 
+
+INSERT INTO employees (first_name, last_name, title, department_name, salary, manager)
+VALUES ("Sarah", "Lourd", "Legal Team Lead", "Legal", 250000, "null"); 
+
+INSERT INTO employees (first_name, last_name, title, department_name, salary, manager)
+VALUES ("Tom", "Allen", "Lawyer", "Legal", 250000, "Sarah Lourd"); 
+
+
+
+
+
+
+
+
+
+
+
+
