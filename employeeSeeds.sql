9b8553013e6e0c092d9be9a61878303c157c3820
Christopher Turton <christopheraturton@gmail.com>
2020-07-13 18:40:30 -0400

    final addition of readme and video

--- employeeSeeds.sql
+++ employeeSeeds.sql
@@ -1,8 +1,8 @@
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("Tom" ,"Allen", 1, 1), ("Sarah", "Lourd", 1, null), ("Kevin", "Tupik", 2, 1),  ("Ashley", "Rodriguez", 2, null), ("Michael", "Chan", 2, 1), ("John", "Doe", 1, null), ("Malia", "Brown", 3, null);
+VALUES ("Tom" ,"Allen", 1, 1), ("Sarah", "Lourd", 1, null), ("Kevin", "Tupik", 2, 1),  ("Ashley", "Rodriguez", 2, null);
 
 INSERT INTO roles (title, salary, department_id) 
-VALUES ("manager", 250000, 1), ("intern", 75000, 1);
+VALUES ("manager", 250000, 1), ("intern", 75000, 1), ("engineer", 120000, 1), ("sales", 78000, 1);
 
 
 INSERT INTO department (name)
